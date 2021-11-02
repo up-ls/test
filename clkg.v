a26b8dac9b89cc12c840ff816377d53326409937
upls <55660435+up-ls@users.noreply.github.com>
2021-11-02 10:48:37 +0800

    FPGA_TEST
    FPGA

---
+++ clkg.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:11:18 05/22/2019 
+// Design Name: 
+// Module Name:    clkg 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clkg(clko,reset,clk);
+    output clko;
+    input reset,clk;
+    reg [5:0] count;
+	 reg clko;
+	 
+
+    always @(posedge clk)
+        begin
+            if (!reset) 
+				   begin
+                count<=0; //同步复位
+					 clko<=1;
+				   end
+            else
+                {clko,count}<=count+1; //计数
+					 
+        end
+		  
+
+
+
+endmodule
